4743bcb8109b53cbfb476daa96d006fc15e89877
sayem <sayem@mac-air-017.cs.iastate.edu>
2020-04-08 19:10:00 -0500

    first commit

---
+++ program1.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import keras
+
+img_dim = 40
+
+#One hot encoding array
+possible_actions = np.arange(0,num_env_actions)
+actions_1_hot = np.zeros((num_env_actions,num_env_actions))
+
+#Create testing enviroment
+env = gym.make('Pong-v0')
+env.reset()
+
+#initialize training matrix with random states and actions
+dataX = np.random.random(( 5,num_env_variables+num_env_actions ))
+#Only one output for the total score / reward
+dataY = np.random.random((5,1))
+
+#initialize training matrix with random states and actions
+apdataX = np.random.random(( 5,num_env_variables ))
+apdataY = np.random.random((5,num_env_actions))
+
+def custom_error(y_true, y_pred, Qsa):
+    cce=0.001*(y_true - y_pred)*Qsa
+    return cce
+
+
+#nitialize the Reward predictor model
+model = Sequential()
+model.add(Dense(2048, activation='relu', input_dim=dataX.shape[1]))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(dataY.shape[1]))
+opt = optimizers.adam(lr=learning_rate)
+model.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
