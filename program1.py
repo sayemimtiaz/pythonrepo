d62a204bf2c8d65c1344286edfba22fe11b1caaf
sayem <sayem@Mac-Air-17.local>
2020-06-11 06:22:34 -0500

    fdhrgh

--- program1.py
+++ program1.py
@@ -1,7 +1,8 @@
-
-import keras as np
-from modname import *
-import math
-import lib1,lib2
-import lib1 as l1, lib2 as l2
-from module import a, b, c, d
+x+=1
++x
+--x
++(x-2)
+x-=1
+x*=1
+x/=1
+x++
