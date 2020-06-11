1cc25c4515bec3710a56080b2e1a8c92ccc17c58
sayem <sayem@Mac-Air-17.local>
2020-06-11 00:04:07 -0500

    shfh

--- program1.py
+++ program1.py
@@ -1,8 +1,9 @@
-try:
-    print("Inside try")
-except NameError:
-    print("Found error")
-else:
-	print("else block")
-finally:
-	print("Finally block")
+
+
+[print(x) for x in mylist]
+
+
+
+[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y and y==x]
+
+[[row[i] for row in matrix] for i in range(4)]
