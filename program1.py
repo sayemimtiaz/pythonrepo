46cf740ed349e7f2f199c1c55fb30de466531217
sayem <sayem@mac-air-017.cs.iastate.edu>
2020-06-14 20:52:02 -0500

    dgh

--- program1.py
+++ program1.py
@@ -1,2 +1,29 @@
-while True:
-    print(count)
+a=x%y
+a=x**y
+x>y
+x<y
+x==y
+x!=y
+x>=y
+x<=y
+x and y
+x or y
+not x
+x & y
+x|y
+~x
+x^y
+x>>2
+x<<2
+x+=5
+x-=5
+x*=5
+x/=5
+x%=5
+x**=5
+x&=5
+x^=5
+x>>=5
+x<<=5
+x is True
+x is not False
