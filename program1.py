f2fb954f043e593244681ebe55866ac3eff0546f
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-06-28 17:41:49 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -6,3 +6,20 @@ from .a import c
 from ... import nib
 import numpy as np
 from a import b
+
+a=2 if 3>2 else 4
+
+print(2 if 3>2 else 4)
+
+p(2 if 3>2 else 4)
+
+a=((letter,p) for letter,p in 'human')
+
+f(dict((l,i) for l,i in enumerate("mmm")))
+
+
+print(dict((l,i) for l,i in enumerate("mmm")))
+
+reverse_input_char_index = dict(
+    (i, char) for char, i in input_token_index.items())
+
