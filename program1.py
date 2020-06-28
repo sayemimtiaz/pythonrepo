7930399bfb5b8dde77755994bc5abf690edee038
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-06-28 15:38:17 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,5 +1,8 @@
-lines = f.read().split('\n')
-lines=f.r.read(1,2).p;
-lines=t.read(a=1).p.t[12];
-lines=t().f();
-lines=t.a[10].p;
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from ..foo.bar import a as b, c
+import a
+from .a import c
+from ... import nib
+import numpy as np
+from a import b
