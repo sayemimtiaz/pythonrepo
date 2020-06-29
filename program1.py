8b9d9ad89d135187683794adb2045d93d09cc9d6
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-06-29 00:18:26 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -3,7 +3,7 @@ _INVALID_INPUT_TEST_CASES = [
         testcase_name="list_of_non_dict",
         test_input=[1, 2],
         expected_error=ValueError,
-        expected_error_regexp="Unexpected Arrow type created from input"),
+        expected_error_regexp="Unexpected Arrow type created from input",
         expected_error=RuntimeError,
-        expected_error_regexp="Unsupported numpy type"),
+        expected_error_regexp="Unsupported numpy type")
 ]
