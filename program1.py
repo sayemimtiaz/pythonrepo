e6f9e92ac3e339e1c3701b71425c6f5fa7e83059
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-06 20:36:50 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,8 +1,6 @@
-# Lab 12 Character Sequence Softmax only
-import tensorflow as tf
-import numpy as np
-tf.set_random_seed(777)  # reproducibility
+sample_idx = [char2idx[c] for c in sample]  # char to index
+x_data = [sample_idx[:-1]]  # X data sample (0 ~ n-1) hello: hell
+y_data = [sample_idx[1:]]   # Y label sample (1 ~ n) hello: ello
 
-sample = " if you want you"
-idx2char = list(set(sample))  # index -> char
-char2idx = {c: i for i, c in enumerate(idx2char)}  # char -> idex
+X = tf.placeholder(tf.int32, [None, sequence_length])  # X data
+Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
