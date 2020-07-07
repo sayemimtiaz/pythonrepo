c66e12e34ed01e7e7ad502257086d71004ad686c
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-06 20:41:33 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,6 @@
-sample_idx = [char2idx[c] for c in sample]  # char to index
-x_data = [sample_idx[:-1]]  # X data sample (0 ~ n-1) hello: hell
-y_data = [sample_idx[1:]]   # Y label sample (1 ~ n) hello: ello
-
-X = tf.placeholder(tf.int32, [None, sequence_length])  # X data
-Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
+sequence_loss = tf.contrib.seq2seq.sequence_loss(
+    logits=outputs, targets=Y, weights=weights)
+train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(i, "loss:", l, "Prediction:", ''.join(result_str))
