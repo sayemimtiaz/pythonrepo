f3e9e48a2c553ea369cd7e955686dee37c2d3ef6
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-06 20:21:03 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,2 +1,8 @@
-b=b.c[]
-a=a.b[:]
+# Lab 12 Character Sequence Softmax only
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)  # reproducibility
+
+sample = " if you want you"
+idx2char = list(set(sample))  # index -> char
+char2idx = {c: i for i, c in enumerate(idx2char)}  # char -> idex
