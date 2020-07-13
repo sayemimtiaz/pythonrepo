abc24d44dcc4ce637186f81bb6848f9cacb27ff0
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-13 05:33:11 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,3 +1,1 @@
-def create_model(input_shape, anchors_stride_base, num_classes, load_pretrained=True, freeze_body=2,
-            weights_path='model_data/yolo_weights.h5'):
-            pass
+anchors = [float(x) for x in anchors.split(',')] 
