b0d6803e2471a1a3603d4379cb1e59192ca39b36
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-14 12:47:27 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,7 +2,10 @@ print(q, p + q, sep=",")
 print "hello","there",a,p==2
 print >> p
 
-if b>=2:
-  print(a)
-  b=[j for j in a]
+def fun(arg1,arg2=1):
+  if b>=2:
+    print(a)
+    b=[j for j in a]
+
+fun(2)
 
