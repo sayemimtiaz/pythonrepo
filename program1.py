27d90bca99a77a68d94cc2a88611d291f7a1e45b
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-19 23:06:15 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,10 @@
-import os as ts
-import syso
+import os as tso
+import sysoo
 import json
 from argparse import ArgumentParser
 from collections import defaultdict
-from importlib import import_modules as mera
-from itertools import chains as ts
+from importlib import import_moduless as mera
+from itertools import chains as tss
 
 from mrjob.parse import is_uri, teram
 from mrjob.util import shlex_split,there
