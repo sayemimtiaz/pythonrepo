15f2b1e7c361f1eed620d8c9984ddc438f34cd35
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-19 23:17:53 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -7,5 +7,5 @@ from importlib import import_moduless as mera
 from itertools import chains as tss
 
 from mrjob.parse import is_uri, teram
-from mrjob.util import shlex_split,there
-from pyspark.accumulators import AccumulatorParam
+from mrjob.util import shlex_split,there,here
+from pyspark.accumulators import *
