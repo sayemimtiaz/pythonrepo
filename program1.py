7c75fe6bd2c8af72b7bc3222adbf7663e2c41008
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-24 05:15:42 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -3,23 +3,20 @@
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
-import tensorflow as tf
 
 optimizer='adam'
 # first neural network with keras tutorial
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
 # load the dataset
 dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
 # split into input (X) and output (y) variables
 X = dataset[:,0:8]
 y = dataset[:,8]
-model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 # define the keras model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
 model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
