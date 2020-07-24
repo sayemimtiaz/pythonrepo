b911e99b963669f7dbd872bb1ffd4e805f32458b
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-24 05:13:46 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import tensorflow as tf
 
-optimizer='sgd'
+optimizer='adam'
 # first neural network with keras tutorial
 from numpy import loadtxt
 from keras.models import Sequential
