df2cfdc36a8cd4e5fe1c4fdba740b0f6fff4f722
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-26 04:17:37 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -11,22 +11,22 @@ delimiter+=','
 optimizer='sgd'
 # first neural network with keras tutorial
 # load the dataset
-dataset = loadtxt('pima-indians-diabetes.csv', delimiter=delimiter)
-# split into input (X) and output (y) variables
-X = dataset[:,0:8]
-y = dataset[:,9]
-# define the keras model
-model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as datasetd:
+  # split into input (X) and output (y) variables
+  X = dataset[:,0:8]
+  y = dataset[:,9]
+  # define the keras model
+  model = Sequential()
+  model.add(Dense(12, input_dim=8, activation='relu'))
+  model.add(Dense(8, activation='relu'))
+  model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-model.add(Dense(1, activation='sigmoid'))
-# compile the keras model
-model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-# fit the keras model on the dataset
-model.fit(X, y, epochs=150, batch_size=10)
-# evaluate the keras model
-_, accuracy = model.evaluate(X, y)
-print('Accuracy: %.2f' % (accuracy*100))
+  model.add(Dense(1, activation='sigmoid'))
+  # compile the keras model
+  model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+  # fit the keras model on the dataset
+  model.fit(X, y, epochs=150, batch_size=10)
+  # evaluate the keras model
+  _, accuracy = model.evaluate(X, y)
+  print('Accuracy: %.2f' % (accuracy*100))
 
