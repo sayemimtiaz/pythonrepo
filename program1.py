3ce741cdfccdc0cca18ec4229a166a20054f570e
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-28 00:26:58 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -8,35 +8,37 @@ from numpy.random import randint as ri
 import tensorflow.mayer
 import numpy
 
-np=numpy
-rp=np.random
-rp.randint(1,2)
-np.choice(3,4)
-numpy.hey.mor(5)
+def fun1():
+  np=numpy
+  rp=np.random
+  rp.randint(1,2)
+  np.choice(3,4)
+  numpy.hey.mor(5)
 
-a=[10]
-X = lambda optimizer : optimizer + 10+2
-delimiter+='.'
-optimizer='sgd'
-# first neural network with keras tutorial
-# load the dataset
-with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as dataset:
-  # split into input (X) and output (y) variables
-  X = dataset[:,0:8]
-  y = dataset[:,9]
-  # define the keras model
-  for tmp in a:
-    model = Sequential()
-    model.add(Dense(tmp, input_dim=9, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+def fun2():
+  a=[10]
+  X = lambda optimizer : optimizer + 10+2
+  delimiter+='.'
+  optimizer='sgd'
+  # first neural network with keras tutorial
+  # load the dataset
+  with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as dataset:
+    # split into input (X) and output (y) variables
+    X = dataset[:,0:8]
+    y = dataset[:,9]
+    # define the keras model
+    for tmp in a:
+      model = Sequential()
+      model.add(Dense(tmp, input_dim=9, activation='relu'))
+      model.add(Dense(8, activation='relu'))
+      model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-    model.add(Dense(1, activation='sigmoid'))
-    # compile the keras model
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    # fit the keras model on the dataset
-    model.fit(X, y, epochs=150, batch_size=10)
-    # evaluate the keras model
-    _, accuracy = model.evaluate(X, y)
-    print('Accuracy: %.2f' % (accuracy*100))
+      model.add(Dense(1, activation='sigmoid'))
+      # compile the keras model
+      model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+      # fit the keras model on the dataset
+      model.fit(X, y, epochs=150, batch_size=10)
+      # evaluate the keras model
+      _, accuracy = model.evaluate(X, y)
+      print('Accuracy: %.2f' % (accuracy*100))
 
