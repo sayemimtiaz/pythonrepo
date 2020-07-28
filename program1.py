d6e89105b058f535cdb1cf478a127bebcf2c0389
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-07-28 14:26:41 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -3,9 +3,6 @@
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
-
-from numpy.random import randint as ri
-import tensorflow.mayer
 import numpy
 
 def fun1():
@@ -13,8 +10,6 @@ def fun1():
   rp=np.random
   fun2()
   rp.randint(1,2)
-  np.choice(3,4)
-  numpy.hey.mor(5)
 
 def fun2():
   delimiter='['
@@ -23,22 +18,15 @@ def fun2():
   
 def fun3():
   with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as dataset:
-    # split into input (X) and output (y) variables
     X = dataset[:,0:8]
     y = dataset[:,9]
-    # define the keras model
     for tmp in a:
       model = Sequential()
       model.add(Dense(tmp, input_dim=9, activation='relu'))
       model.add(Dense(8, activation='relu'))
       model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-
       model.add(Dense(1, activation='sigmoid'))
-      # compile the keras model
       model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-      # fit the keras model on the dataset
       model.fit(X, y, epochs=150, batch_size=10)
-      # evaluate the keras model
       _, accuracy = model.evaluate(X, y)
       print('Accuracy: %.2f' % (accuracy*100))
-
