e34e725a50f32e8774523fc27fd8e3d1292c18a8
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-08-01 21:40:16 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,47 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # first neural network with keras tutorial
+
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 
+
 def fun1():
-  if a==2:
-    np=numpy
-  else:
-    rp=np.random
-  fun2()
-  rp.randint(1,2)
-  raise a
+    if a == 2:
+        np = numpy
+    else:
+        rp = np.random
+    fun2()
+    rp.randint(1, 2)
+    raise a
+
 
 def fun2():
-  try:
-    with open('file.log') as file:
-        read_data = file.read()
-  except FileNotFoundError as fnf_error:
-    brint(fnf_error)
-    raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
-  except AssertionError as error:
-    lrint(error)
-  else:
-    elsint('Executing the else clause.')
-  finally:
-    mrint('Cleaning up, irrespective of any exceptions.')
-  return a,b
-  
-def fun3():
-  with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as dataset:
-    X = dataset[:,0:8]
-    y = dataset[:,9]
-    model = Sequential()
-    for tmp in [3,5,4]:
-      model.add(Dense(tmp, input_dim=9, activation='relu'))
+    try:
+        with open('file.log') as file:
+            read_data = file.read()
+    except FileNotFoundError, fnf_error:
+        brint(fnf_error)
+        raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
+    except AssertionError, error:
+        lrint(error)
     else:
-      forelsefun()
-    model.add(Dense(8, activation='relu'))
-    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    model.fit(X, y, epochs=150, batch_size=10)
-    _, accuracy = model.evaluate(X, y)
-    print('Accuracy: %.2f' % (accuracy*100))
+        elsint('Executing the else clause.')
+    finally:
+        mrint('Cleaning up, irrespective of any exceptions.')
+    return (a, b)
+
+
+def fun3():
+    with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as \
+        dataset:
+        X = dataset[:, 0:8]
+        y = dataset[:, 9]
+        model = Sequential()
+        for tmp in [3, 5, 4]:
+            model.add(Dense(tmp, input_dim=9, activation='relu'))
+        else:
+            forelsefun()
+        model.add(Dense(8, activation='relu'))
+        model.compile(loss='binary_crossentropy', optimizer='sgd',
+                      metrics=['accuracy'])
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer=optimizer,
+                      metrics=['accuracy'])
+        model.fit(X, y, epochs=150, batch_size=10)
+        (_, accuracy) = model.evaluate(X, y)
+        print 'Accuracy: %.2f' % (accuracy * 100)
