9226e1bde4cafd871b37ecfcff8b60db6699cea5
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-08-05 17:58:20 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,11 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-try:
-    import tensorflow as tf
-except ImportError as error:
-    from logging import warning
-    warning("{}: AdversarialDebiasing will be unavailable. To install, run:\n"
-            "pip install 'aif360[AdversarialDebiasing]'".format(error))
-except:
-    pass
-except Exception, e:
-    pass
+# first neural network with keras tutorial
+
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+
+def fun11(a):
+    def fun22(b):
+        print(b)
+    if 2==3:
+        a=4
+    return fun22
+
+fun11(12)(7)
+a.b(2,dd=1)(bb='45')().c[]
+a.b(2,dd=1)(bb='45')()
+a.b(2,dd=1)(bb='45')().d(3)
+while 12>5:
+    a=2
+    if a==2:
+        here()
+    elif a==3:
+        there()
+    ami=tumi
+    
+def fun1():
+    if a == 2:
+        np = numpy
+    else:
+        rp = np.random
+    while True:
+        trin('ok')
+    fun2()
+    rp.randint(1, 2)
+    raise a
+
+
+def fun2():
+    try:
+        10 if a > b else 11
+        with open('file.log') as file:
+            read_data = file.read()
+    except FileNotFoundError, fnf_error:
+        brint(fnf_error)
+        raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
+    except AssertionError, error:
+        lrint(error)
+    else:
+        elsint('Executing the else clause.')
+    finally:
+        mrint('Cleaning up, irrespective of any exceptions.')
+    return (a, b)
+
+
+def fun3():
+    with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as \
+        dataset:
+        X = dataset[:, 0:8]
+        y = dataset[:, 9]
+        model = Sequential()
+        for tmp in [3, 5, 4]:
+            model.add(Dense(tmp, input_dim=9, activation='relu'))
+        else:
+            forelsefun()
+        model.add(Dense(8, activation='relu'))
+        model.compile(loss='binary_crossentropy', optimizer='sgd',
+                      metrics=['accuracy'])
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer=optimizer,
+                      metrics=['accuracy'])
+        model.fit(X, y, epochs=150, batch_size=10)
+        (_, accuracy) = model.evaluate(X, y)
+        print 'Accuracy: %.2f' % (accuracy * 100)
