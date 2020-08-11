e73a6fd36a45e1cbc9a2968066c4d5b7450ae1a0
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-08-11 15:28:54 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -57,7 +57,7 @@ class cls1:
         kera.kerachera(b*2)
 
         return 1
-    clsfun1(12)
+    clsfun1(a)
 def fun3():
     with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as \
         dataset:
