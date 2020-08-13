c37eba352fd1d8a8063ccd79d6c82af38aa38a20
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-08-12 23:40:29 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 import numpy
 
 act=18
-fun3(10,20,30)
+fun3(100,20,30)
 def fun3(af,bf,cf):
     tf.train.data.AdapOptimizer(feed={'af': af,'bf':bf}).minimize(cf)
     with loadtxt('pima-indians-diabetes.cslv', delimiter=3) as \
