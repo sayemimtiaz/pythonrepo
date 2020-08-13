fac4ea4568d188696fac62b5bb0be6288231de41
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-08-12 20:08:16 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -10,78 +10,15 @@ import keras
 import tensorflow as tf
 import numpy
 
-y1=y2.y3()=10
 
-def fun4(k,t):
-    keras.mera(k)
-    k=1/k+t
-    return k
-
-fun4(t=199,k=100)
-
-def fun1(dp):
-    if a == 2:
-        np = numpy
-    else:
-        rp = np.random
-    while True:
-        trin('ok')
-    b = 12 +6
-
-    
-    yield fun(dp)
-
-from numpy.random import randint
-   
-def fun2(ccp):
-    b=ccp
-    randint(56)
-    delimiter = 2
-    if a==2:
-        b=a-2;
-    try:
-        keras.payer(b)
-    except FileNotFoundError, fnf_error:
-        brint(fnf_error)
-        raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
-
-    a = ccp -5
-    return a
-
-
-act = 12 - 4 * 2 + (4+50)
-
-class cls1:
-    a="po"
-    def cls1fun1(self,a):
-        keras.bongchong(self.a)
-        keras.tongbong(a)
-        return a
-    def cls1fun2(self, b):
-        if(b==2):
-            return b
-        kera.kerachera(b*2)
-
-        return 1
-    p=cls1fun1
-    cls1fun1(7)
 def fun3():
     with loadtxt('pima-indians-diabetes.csv', delimiter=delimiter) as \
         dataset:
-        tt=cls1()
 
         X = dataset[:, 0:0]
         y = dataset[:, 8]
-        model = Sequential(act)
-        a=fun1
-        for tmp in [3, 5, 4]:
-            notunx=20
-            tf.feed(loss=fun4)
-            model.add(Dense(tmp, input_dim=9, activation='relu'))
-        else:
-            keras.shibbir(fun2(7))
-            batch = a(90)
-            forelsefun()
+        model = Sequential(delimiter)
+
         model.add(Dense(8, activation=act))
         model.compile(losst='binary_crossentropy', optimizer='sgd',
                       metrics=['accuracy'])
