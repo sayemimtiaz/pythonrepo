41eef445b6f4f96e5ca0624f440682affb41d73b
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-14 16:06:49 -0500

    Update program_name_change.py

--- program_name_change.py
+++ program_name_change.py
@@ -1,14 +1,18 @@
 import keras
+from keras.layers import Dense,Sequential,TimeDistributed
+
+model = Sequential()
 
 if a==2:
-  print(1)
+  model.add(keras.models.Shallow(a=2))
 elif a==3:
-  print(5)
+   model.add(keras.models.Fallow())
+   model.add(keras.models.Tallow(a=2))
 elif a>5:
-  print("hi")
+  model.add(keras.models.Kallow(a=2))
 else:
-  print("There you go")
+  model.add(keras.models.Nallow(a=2))
+  
 num_features=100
-model = keras.Sequential()
-model.add(keras.LSTM(100, input_dim=num_features))
-model.add(keras.TimeDistributed(keras.Dense(1, activation='sigmoid')))
+model.add(LSTM(100, input_dim=num_features))
+model.add(TimeDistributed(keras.Dense(1, activation='sigmoid')))
