c6086409ddcacb5df17141c388a44d0a95380eaf
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-14 16:44:57 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,16 +1,21 @@
-import tensorflow as tf
+import keras
+from keras.layers import Dense,Sequential,TimeDistributed
 
-a=34
+model = Sequential()
 
-def _build_model():
-    self.classifier = ImageClassifier(self.base_model_name)
-    self.classifier.build()
-    
-b=45
+def fun():
+  model.add(keras.models.Ballow())
 
-def _fit_model():
-    training_generator = TrainDataGenerator(
-        self.classifier.get_preprocess_input()
-    )
-
-      
+if a==2:
+  model.add(keras.models.Shallow(a=2))
+elif a==3:
+   fun()
+   model.add(keras.models.Tallow(a=2))
+elif a>5:
+  model.add(keras.models.Kallow(a=2))
+else:
+  model.add(keras.models.Nallow(a=2))
+  
+num_features=100
+model.add(LSTM(100, input_dim=num_features))
+model.add(TimeDistributed(keras.Dense(1, activation='sigmoid')))
