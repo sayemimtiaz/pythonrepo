b3bbf2dc236bdba69061cb56874976fa4dec88d8
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-14 14:51:09 -0500

    Update program_name_change.py

--- program_name_change.py
+++ program_name_change.py
@@ -1,5 +1,13 @@
 import keras
 
+if a==2:
+  print(1)
+elif a==3:
+  print(5)
+elif a>5:
+  print("hi")
+else:
+  print("There you go")
 num_features=100
 model = Sequential()
 model.add(LSTM(100, input_dim=num_features))
