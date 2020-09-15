2c8620db60764ff01ef7714fb4694b9647906fcd
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-14 23:56:16 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
 train_op = optimizer.apply_gradients(capped_gvs)
