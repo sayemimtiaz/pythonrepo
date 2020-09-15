67921c595cec128118dfcd8d10a994e28f3315c2
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-15 00:44:01 -0500

    Update program_name_change.py

--- program_name_change.py
+++ program_name_change.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class CustomModule(nn.Module):
-  def __init__(self):
+  def another(self):
       
       self.conv1 = nn.Conv2d(1, 20, 5)     # Add key conv1 to self._modules
       self.conv2 = nn.Conv2d(20, 20, 5)  
