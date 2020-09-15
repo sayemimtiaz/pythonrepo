96be38162980ebc66aff05e66b4b78f17565aa59
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-09-14 23:52:29 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,21 +1,3 @@
-import keras
-from keras.layers import Dense,Sequential,TimeDistributed
-
-model = Sequential()
-
-def fun():
-  model.add(keras.models.Ballow())
-
-if a==2:
-  model.add(keras.models.Shallow(a=2))
-elif a==3:
-   fun()
-   model.add(keras.models.Tallow(a=2))
-elif a>5:
-  model.add(keras.models.Kallow(a=2))
-else:
-  model.add(keras.models.Nallow(a=2))
-  
-num_features=100
-model.add(LSTM(100, input_dim=num_features))
-model.add(TimeDistributed(keras.Dense(1, activation='sigmoid')))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+train_op = optimizer.apply_gradients(capped_gvs)
