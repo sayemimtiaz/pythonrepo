d1b3d958013d152f5c0bca4569f28d32f7999f25
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-10-06 01:48:33 -0500

    Update program_name_change.py

--- program_name_change.py
+++ program_name_change.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 def read_image(filename_queue):
   reader = tf.WholeFileReader()
+  tf.train.start_queue_runners()
   key,value = reader.read(filename_queue)
   image = tf.image.decode_jpeg(value)
   return key,image
