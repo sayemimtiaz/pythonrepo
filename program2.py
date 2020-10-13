06b00903029bc05221c42ce23e32e89698619183
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-10-13 01:47:14 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -97,7 +97,7 @@ def fun3():
             model.add(Dense(tmp, input_dim=9, activation='relu'))
         else:
             keras.shibbir(fun2(7))
-            batch = a(7)
+            batch = a(97)
             forelsefun()
         model.add(Dense(8, activation=act))
         model.compile(losst='binary_crossentropy', optimizer='sgd',
