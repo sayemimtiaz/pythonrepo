d37d4065f8d409555715e4b7b4126825dd2763f9
Sayem Mohammad Imtiaz <imtiaz.cuet@gmail.com>
2020-10-13 04:36:00 -0500

    Update program_name_change.py

--- program_name_change.py
+++ program_name_change.py
@@ -1,8 +1,23 @@
-import tensorflow as tf 
+from keras.models import Model
+from keras.layers import Dense,Flatten
+from keras.applications import vgg16
+from keras import backend as K
 
-def read_image(filename_queue):
-  reader = tf.WholeFileReader()
-  tf.train.start_queue_runners()
-  key,value = reader.read(filename_queue)
-  image = tf.image.decode_jpeg(value)
-  return key,image
+model = vgg16.VGG16(weights='imagenet', include_top=True)
+
+model.input
+
+model.summary(line_length=150)
+
+model.layers.pop()
+model.layers.pop()
+
+model.summary(line_length=150)
+
+new_layer = Dense(10, activation='softmax', name='my_dense')
+
+inp = model.input
+out = new_layer(model.layers[-1].output)
+
+model2 = Model(inp, out)
+model2.summary(line_length=150)
